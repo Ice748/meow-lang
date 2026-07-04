2c88d8e387552b669c69dda044682428b0032ead
Ice748 <vinogradovsergeyche@gmail.com>
2026-07-04 10:12:57 +0300

    Update main.c
    Add 'new' command

--- src/main.c
+++ src/main.c
@@ -22,6 +22,28 @@ int main(int argc, char const *argv[]) {
     return run(file);
   }
 
+  if (strcmp(argv[1], "new") == 0) {
+    if (argc > 2) {
+      printf("%s\n", ERROR_002); // ERROR 002: Too much arguments.
+      return 1;
+    }
+
+    FILE *file = fopen("example.meow", "wx");
+    if (file == NULL) {
+      printf("%s\n", ERROR_004); // ERROR 004: Cannot create file.
+      return 1;
+    }
+
+    fprintf(
+      file,
+      "@import(\"console\")\n"
+      "console.println(\"Hello World!\")\n"
+    );
+
+    fclose(file);
+    return 0;
+  }
+
   if (strcmp(argv[1], "version") == 0) {
     if (argc > 2) {
       printf("%s\n", ERROR_002); // ERROR 002: Too much arguments.
