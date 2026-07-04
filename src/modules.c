064bad045100e957e15fad1b31a0a2a1f7867898
Ice748 <vinogradovsergeyche@gmail.com>
2026-07-04 20:01:30 +0300

    Create modules.c

---
+++ src/modules.c
@@ -0,0 +1,27 @@
+#include "modules/console.h"
+
+void println(char *arg) {
+  printf("%s\n", arg);
+}
+
+void print(char *arg) {
+  printf("%s", arg);
+}
+
+int console_module(char *ptr) {
+  char println_arg = getArgString(ptr);
+  if (println_arg != NULL) {
+    println(println_arg);
+    free(println_arg);
+    return 1;
+  }
+
+  char print_arg = getArgString(ptr);
+  if (print_arg != NULL) {
+    print(print_arg);
+    free(print_arg);
+    return 1;
+  }
+
+  return 0;
+}
