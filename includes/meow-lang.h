778864c560ccc48963583c9d657f4bf63c79d418
Ice748 <vinogradovsergeyche@gmail.com>
2026-07-05 12:34:48 +0300

    Create meow-lang.h

---
+++ includes/meow-lang.h
@@ -0,0 +1,12 @@
+#ifndef MEOW_LANG_H
+#define MEOW_LANG_H
+
+#define VERSION "v0.0.1 beta"
+#define ERROR_001 "ERROR 001: Too few arguments."
+#define ERROR_002 "ERROR 002: Too much arguments."
+#define ERROR_003 "ERROR 003: Unknown command."
+#define ERROR_004 "ERROR 004: Cannot create file."
+#define ERROR_005 "ERROR 005: Cannot open file."
+#define ERROR_006 "ERROR 006: Unknown module."
+
+#endif
