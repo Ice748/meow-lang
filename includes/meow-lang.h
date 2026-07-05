35273b4d351ff3b1d7376ce66c0e238ae1fb56b7
Ice748 <vinogradovsergeyche@gmail.com>
2026-07-05 12:42:24 +0300

    Update meow-lang.h

--- includes/meow-lang.h
+++ includes/meow-lang.h
@@ -2,11 +2,20 @@
 #define MEOW_LANG_H
 
 #define VERSION "v0.0.1 beta"
+
 #define ERROR_001 "ERROR 001: Too few arguments."
-#define ERROR_002 "ERROR 002: Too much arguments."
+#define ERROR_002 "ERROR 002: Too many arguments."
 #define ERROR_003 "ERROR 003: Unknown command."
 #define ERROR_004 "ERROR 004: Cannot create file."
 #define ERROR_005 "ERROR 005: Cannot open file."
 #define ERROR_006 "ERROR 006: Unknown module."
+
+#define HELP
+  "run <file name>: Run file."\
+  "new: Create new .meow file."\
+  "version: Write interpreter version."\
+  "help: Write this list."\
+  "docs: Write docs."
+#define DOCS "Nothing here yet..."
 
 #endif
