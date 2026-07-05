f3c5df6cea737c22e5a6fa01c368bf23dd648205
Ice748 <vinogradovsergeyche@gmail.com>
2026-07-05 12:26:21 +0300

    Update interpreter.c
    Fix a lot of bugs

--- src/interpreter.c
+++ src/interpreter.c
@@ -2,14 +2,16 @@
 
 char *getArgString(char *ptr, const char *module, const char *function);
 
-int run(FILE *file) {
+int run(char *file_name) {
+  FILE *file = fopen(file_name, "r");
+
   if (file == NULL) {
     printf("%s\n", ERROR_005); // ERROR 005: Cannot open file.
     return 1;
   }
 
   int modules[1]; // 0: console
-  for (size_t idx = 0; idx < sizeof(modules); idx++) {
+  for (size_t idx = 0; idx < sizeof(modules) / sizeof(modules[0]); idx++) {
     modules[idx] = 0;
   }
 
@@ -19,30 +21,53 @@ int run(FILE *file) {
     buffer[strcspn(buffer, "\n")] = '\0';
     line++;
 
-    char *import_args = getArgString(buffer, NULL, "@import");
-    char *deimport_args = getArgString(buffer, NULL, "@deimport");
-    if (import_args != NULL || deimport_args != NULL) {
-      if (strcmp(import_args, "console") modules[0] = import_args ? 1 : 0;
+    char *import_arg = getArgString(buffer, NULL, "@import");
+    if (import_arg != NULL) {
+      if (strcmp(import_arg, "console") == 0) modules[0] = 1;
       else {
-        printf("Error in line %d:\n%s\n", line, ERROR_006); // ERROR 006: Unknown module.
+        printf("Error in %s in line %d:\n%s\n", file_name, line, ERROR_006); // ERROR 006: Unknown module.
+        free(import_arg);
+        fclose(file);
         return 1;
       }
 
-      free(import_args ? import_args : deimport_args);
+      free(import_arg);
       continue;
     }
 
-    char *launch_args = getArgString(buffer, NULL, "@launch");
-    if (launch_args != NULL) {
-      FILE *launch_file = fopen(launch_args, "r");
-      run(launch_file);
+    char *deimport_arg = getArgString(buffer, NULL, "@deimport");
+    if (deimport_arg != NULL) {
+      if (strcmp(deimport_arg, "console") == 0) modules[0] = 0;
+      else {
+        printf("Error in %s in line %d:\n%s\n", file_name, line, ERROR_006); // ERROR 006: Unknown module.
+        free(deimport_arg);
+        fclose(file);
+        return 1;
+      }
+
+      free(deimport_arg);
+      continue;
+    }
+
+    char *launch_arg = getArgString(buffer, NULL, "@launch");
+    if (launch_arg != NULL) {
+      if (run(launch_arg) == 0) {
+        free(launch_arg);
+        continue;
+      } else {
+        free(launch_arg);
+        fclose(file);
+        return 1;
+      }
+      continue;
     }
 
     if (modules[0]) {
-      console_module();
+      if (console_module() == 1) continue;
     }
 
-    printf("Error in line %d\n", line);
+    printf("Error in %s in line %d\n", file_name, line);
+    fclose(file);
     return 1;
   }
 
