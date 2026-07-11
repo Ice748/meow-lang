fd1072eae9c6d944113c76437229d7c6e6f81ffa
Ice748 <vinogradovsergeyche@gmail.com>
2026-07-11 12:34:43 +0300

    Update console.h
    functions list:
    println(char *arg);
    print(char *arg);
    input(char *arg);
    get_char(char *arg);
    clear().

--- includes/modules/console.h
+++ includes/modules/console.h
@@ -3,10 +3,15 @@
 
 #include <stdio.h>
 #include <stdlib.h>
+#include <conio.h>
 #include "interpreter.h"
 
 void println(char *arg);
 void print(char *arg);
+void input(char *arg);
+void get_char(char *arg);
+void clear(void);
+
 int console_module(char *ptr);
 
 #endif
